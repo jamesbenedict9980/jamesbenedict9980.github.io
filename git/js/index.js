c57b0bf3f3bb23c3086054ac3afe1d0707b7fa8e
James Benedict <jamesbenedict.9980@gmail.com>
2019-02-26 00:38:50 -0800

    Add files via upload

--- git/js/index.js
+++ git/js/index.js
@@ -111,7 +111,7 @@ function currentDivs1(m1) {
 
 function showDiv1(m1) {
 var j1;
-console.log(m1);
+console.log(out);
 var y1 = document.getElementsByClassName("mySlides");
   if(m1 > y1.length){$('#out').html('1' +' of 8');} 
  else if( m1 >= 1) {$('#out').html(m1 +' of 8');}
