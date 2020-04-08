52ffb63aad499374e1c09faa9c216cbf163f09b9
James Benedict <jamesbenedict.9980@gmail.com>
2020-04-07 22:13:40 -0700

    Add files via upload

---
+++ bol/js/nextgen-topnav-and-footer.js
@@ -0,0 +1,121 @@
+$(document).ready(function () {
+    LoadMenu();
+    LoadFooter();
+});
+function LoadMenu() {
+    var commonTopNavbar = '<div id="top-navbar" class="navbar navbar-dark bg-dark navbar-expand-lg flex-column flex-md-row">\
+    <span class="navbar-brand-special"><a href="index.html"><span class="ml-4 pl-2 fw-bold"><img src="img/msi-dev-guide-logo.png" alt="logo" height="50" width="50"></span> <span class="text-muted">|</span> Item Master Data</a></span>\
+    <button class="btn btn-dark d-inline-block d-lg-none ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">\
+        <i class="fas fa-angle-double-left"></i>\
+    </button>\
+        <!-- Drop Down Menu -->\
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">\
+            <ul class="navbar-nav ml-auto">\
+                <li class="nav-item active mr-4">\
+                    <a class="nav-link" href="#">\
+                        <!-- US Flag SVG -->\
+<svg width="40px" height="24px" viewBox="0 0 512 300" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">\
+<g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">\
+    <g id="iconfinder_274_Ensign_Flag_Nation_states_2634451">\
+        <rect id="Rectangle" fill="#CB001C" x="0" y="276.4" width="512" height="23"></rect>\
+        <rect id="Rectangle" fill="#FFFFFF" x="0" y="253.4" width="512" height="23"></rect>\
+        <rect id="Rectangle" fill="#CB001C" x="0" y="230.4" width="512" height="23"></rect>\
+        <rect id="Rectangle" fill="#FFFFFF" x="0" y="207.4" width="512" height="23"></rect>\
+        <rect id="Rectangle" fill="#CB001C" x="0" y="184.5" width="512" height="23"></rect>\
+        <rect id="Rectangle" fill="#FFFFFF" x="0" y="161.5" width="512" height="23"></rect>\
+        <rect id="Rectangle" fill="#CB001C" x="0" y="138.5" width="512" height="23"></rect>\
+        <rect id="Rectangle" fill="#FFFFFF" x="0" y="115.5" width="512" height="23"></rect>\
+        <rect id="Rectangle" fill="#CB001C" x="0" y="92.6" width="512" height="23"></rect>\
+        <rect id="Rectangle" fill="#FFFFFF" x="0" y="69.6" width="512" height="23"></rect>\
+        <rect id="Rectangle" fill="#CB001C" x="0" y="46.6" width="512" height="23"></rect>\
+        <rect id="Rectangle" fill="#FFFFFF" x="0" y="23.6" width="512" height="23"></rect>\
+        <rect id="Rectangle" fill="#CB001C" x="0" y="0.7" width="512" height="23"></rect>\
+        <rect id="Rectangle" fill="#2A3560" x="0" y="0.7" width="233.8" height="160.8"></rect>\
+        <polygon id="Path" fill="#FFFFFF" points="21.1 18.4 23.1 24.5 29.4 24.5 24.3 28.2 26.2 34.3 21.1 30.5 15.9 34.3 17.9 28.2 12.7 24.5 19.1 24.5"></polygon>\
+        <polygon id="Path" fill="#FFFFFF" points="21.1 45.2 23.1 51.3 29.4 51.3 24.3 55.1 26.2 61.1 21.1 57.4 15.9 61.1 17.9 55.1 12.7 51.3 19.1 51.3"></polygon>\
+        <polygon id="Path" fill="#FFFFFF" points="21.1 72.1 23.1 78.2 29.4 78.2 24.3 81.9 26.2 88 21.1 84.2 15.9 88 17.9 81.9 12.7 78.2 19.1 78.2"></polygon>\
+        <polygon id="Path" fill="#FFFFFF" points="21.1 98.9 23.1 105 29.4 105 24.3 108.8 26.2 114.8 21.1 111.1 15.9 114.8 17.9 108.8 12.7 105 19.1 105"></polygon>\
+        <polygon id="Path" fill="#FFFFFF" points="21.1 125.8 23.1 131.8 29.4 131.8 24.3 135.6 26.2 141.7 21.1 137.9 15.9 141.7 17.9 135.6 12.7 131.8 19.1 131.8"></polygon>\
+        <polygon id="Path" fill="#FFFFFF" points="39.8 32.5 41.8 38.6 48.1 38.6 43 42.4 44.9 48.4 39.8 44.7 34.6 48.4 36.6 42.4 31.4 38.6 37.8 38.6"></polygon>\
+        <polygon id="Path" fill="#FFFFFF" points="39.8 59.4 41.8 65.4 48.1 65.4 43 69.2 44.9 75.3 39.8 71.5 34.6 75.3 36.6 69.2 31.4 65.4 37.8 65.4"></polygon>\
+        <polygon id="Path" fill="#FFFFFF" points="39.8 86.2 41.8 92.3 48.1 92.3 43 96.1 44.9 102.1 39.8 98.4 34.6 102.1 36.6 96.1 31.4 92.3 37.8 92.3"></polygon>\
+        <polygon id="Path" fill="#FFFFFF" points="39.8 113.1 41.8 119.1 48.1 119.1 43 122.9 44.9 129 39.8 125.2 34.6 129 36.6 122.9 31.4 119.1 37.8 119.1"></polygon>\
+        <polygon id="Path" fill="#FFFFFF" points="58.7 18.4 60.7 24.5 67.1 24.5 61.9 28.2 63.9 34.3 58.7 30.5 53.5 34.3 55.5 28.2 50.3 24.5 56.7 24.5"></polygon>\
+        <polygon id="Path" fill="#FFFFFF" points="58.7 45.2 60.7 51.3 67.1 51.3 61.9 55.1 63.9 61.1 58.7 57.4 53.5 61.1 55.5 55.1 50.3 51.3 56.7 51.3"></polygon>\
+        <polygon id="Path" fill="#FFFFFF" points="58.7 72.1 60.7 78.2 67.1 78.2 61.9 81.9 63.9 88 58.7 84.2 53.5 88 55.5 81.9 50.3 78.2 56.7 78.2"></polygon>\
+        <polygon id="Path" fill="#FFFFFF" points="58.7 98.9 60.7 105 67.1 105 61.9 108.8 63.9 114.8 58.7 111.1 53.5 114.8 55.5 108.8 50.3 105 56.7 105"></polygon>\
+        <polygon id="Path" fill="#FFFFFF" points="58.7 125.8 60.7 131.8 67.1 131.8 61.9 135.6 63.9 141.7 58.7 137.9 53.5 141.7 55.5 135.6 50.3 131.8 56.7 131.8"></polygon>\
+        <polygon id="Path" fill="#FFFFFF" points="77.4 32.5 79.4 38.6 85.8 38.6 80.6 42.4 82.6 48.4 77.4 44.7 72.2 48.4 74.2 42.4 69 38.6 75.4 38.6"></polygon>\
+        <polygon id="Path" fill="#FFFFFF" points="77.4 59.4 79.4 65.4 85.8 65.4 80.6 69.2 82.6 75.3 77.4 71.5 72.2 75.3 74.2 69.2 69 65.4 75.4 65.4"></polygon>\
+        <polygon id="Path" fill="#FFFFFF" points="77.4 86.2 79.4 92.3 85.8 92.3 80.6 96.1 82.6 102.1 77.4 98.4 72.2 102.1 74.2 96.1 69 92.3 75.4 92.3"></polygon>\
+        <polygon id="Path" fill="#FFFFFF" points="77.4 113.1 79.4 119.1 85.8 119.1 80.6 122.9 82.6 129 77.4 125.2 72.2 129 74.2 122.9 69 119.1 75.4 119.1"></polygon>\
+        <polygon id="Path" fill="#FFFFFF" points="96.3 18.4 98.3 24.5 104.7 24.5 99.5 28.2 101.5 34.3 96.3 30.5 91.2 34.3 93.2 28.2 88 24.5 94.4 24.5"></polygon>\
+        <polygon id="Path" fill="#FFFFFF" points="96.3 45.2 98.3 51.3 104.7 51.3 99.5 55.1 101.5 61.1 96.3 57.4 91.2 61.1 93.2 55.1 88 51.3 94.4 51.3"></polygon>\
+        <polygon id="Path" fill="#FFFFFF" points="96.3 72.1 98.3 78.2 104.7 78.2 99.5 81.9 101.5 88 96.3 84.2 91.2 88 93.2 81.9 88 78.2 94.4 78.2"></polygon>\
+        <polygon id="Path" fill="#FFFFFF" points="96.3 98.9 98.3 105 104.7 105 99.5 108.8 101.5 114.8 96.3 111.1 91.2 114.8 93.2 108.8 88 105 94.4 105"></polygon>\
+        <polygon id="Path" fill="#FFFFFF" points="96.3 125.8 98.3 131.8 104.7 131.8 99.5 135.6 101.5 141.7 96.3 137.9 91.2 141.7 93.2 135.6 88 131.8 94.4 131.8"></polygon>\
+        <polygon id="Path" fill="#FFFFFF" points="115 32.5 117 38.6 123.4 38.6 118.2 42.4 120.2 48.4 115 44.7 109.9 48.4 111.8 42.4 106.7 38.6 113.1 38.6"></polygon>\
+        <polygon id="Path" fill="#FFFFFF" points="115 59.4 117 65.4 123.4 65.4 118.2 69.2 120.2 75.3 115 71.5 109.9 75.3 111.8 69.2 106.7 65.4 113.1 65.4"></polygon>\
+        <polygon id="Path" fill="#FFFFFF" points="115 86.2 117 92.3 123.4 92.3 118.2 96.1 120.2 102.1 115 98.4 109.9 102.1 111.8 96.1 106.7 92.3 113.1 92.3"></polygon>\
+        <polygon id="Path" fill="#FFFFFF" points="115 113.1 117 119.1 123.4 119.1 118.2 122.9 120.2 129 115 125.2 109.9 129 111.8 122.9 106.7 119.1 113.1 119.1"></polygon>\
+        <polygon id="Path" fill="#FFFFFF" points="134 18.4 136 24.5 142.3 24.5 137.2 28.2 139.1 34.3 134 30.5 128.8 34.3 130.8 28.2 125.6 24.5 132 24.5"></polygon>\
+        <polygon id="Path" fill="#FFFFFF" points="134 45.2 136 51.3 142.3 51.3 137.2 55.1 139.1 61.1 134 57.4 128.8 61.1 130.8 55.1 125.6 51.3 132 51.3"></polygon>\
+        <polygon id="Path" fill="#FFFFFF" points="134 72.1 136 78.2 142.3 78.2 137.2 81.9 139.1 88 134 84.2 128.8 88 130.8 81.9 125.6 78.2 132 78.2"></polygon>\
+        <polygon id="Path" fill="#FFFFFF" points="134 98.9 136 105 142.3 105 137.2 108.8 139.1 114.8 134 111.1 128.8 114.8 130.8 108.8 125.6 105 132 105"></polygon>\
+        <polygon id="Path" fill="#FFFFFF" points="134 125.8 136 131.8 142.3 131.8 137.2 135.6 139.1 141.7 134 137.9 128.8 141.7 130.8 135.6 125.6 131.8 132 131.8"></polygon>\
+        <polygon id="Path" fill="#FFFFFF" points="152.7 32.5 154.6 38.6 161 38.6 155.9 42.4 157.8 48.4 152.7 44.7 147.5 48.4 149.5 42.4 144.3 38.6 150.7 38.6"></polygon>\
+        <polygon id="Path" fill="#FFFFFF" points="152.7 59.4 154.6 65.4 161 65.4 155.9 69.2 157.8 75.3 152.7 71.5 147.5 75.3 149.5 69.2 144.3 65.4 150.7 65.4"></polygon>\
+        <polygon id="Path" fill="#FFFFFF" points="152.7 86.2 154.6 92.3 161 92.3 155.9 96.1 157.8 102.1 152.7 98.4 147.5 102.1 149.5 96.1 144.3 92.3 150.7 92.3"></polygon>\
+        <polygon id="Path" fill="#FFFFFF" points="152.7 113.1 154.6 119.1 161 119.1 155.9 122.9 157.8 129 152.7 125.2 147.5 129 149.5 122.9 144.3 119.1 150.7 119.1"></polygon>\
+        <polygon id="Path" fill="#FFFFFF" points="171.6 18.4 173.6 24.5 180 24.5 174.8 28.2 176.8 34.3 171.6 30.5 166.4 34.3 168.4 28.2 163.2 24.5 169.6 24.5"></polygon>\
+        <polygon id="Path" fill="#FFFFFF" points="171.6 45.2 173.6 51.3 180 51.3 174.8 55.1 176.8 61.1 171.6 57.4 166.4 61.1 168.4 55.1 163.2 51.3 169.6 51.3"></polygon>\
+        <polygon id="Path" fill="#FFFFFF" points="171.6 72.1 173.6 78.2 180 78.2 174.8 81.9 176.8 88 171.6 84.2 166.4 88 168.4 81.9 163.2 78.2 169.6 78.2"></polygon>\
+        <polygon id="Path" fill="#FFFFFF" points="171.6 98.9 173.6 105 180 105 174.8 108.8 176.8 114.8 171.6 111.1 166.4 114.8 168.4 108.8 163.2 105 169.6 105"></polygon>\
+        <polygon id="Path" fill="#FFFFFF" points="171.6 125.8 173.6 131.8 180 131.8 174.8 135.6 176.8 141.7 171.6 137.9 166.4 141.7 168.4 135.6 163.2 131.8 169.6 131.8"></polygon>\
+        <polygon id="Path" fill="#FFFFFF" points="190.3 32.5 192.3 38.6 198.7 38.6 193.5 42.4 195.5 48.4 190.3 44.7 185.1 48.4 187.1 42.4 181.9 38.6 188.3 38.6"></polygon>\
+        <polygon id="Path" fill="#FFFFFF" points="190.3 59.4 192.3 65.4 198.7 65.4 193.5 69.2 195.5 75.3 190.3 71.5 185.1 75.3 187.1 69.2 181.9 65.4 188.3 65.4"></polygon>\
+        <polygon id="Path" fill="#FFFFFF" points="190.3 86.2 192.3 92.3 198.7 92.3 193.5 96.1 195.5 102.1 190.3 98.4 185.1 102.1 187.1 96.1 181.9 92.3 188.3 92.3"></polygon>\
+        <polygon id="Path" fill="#FFFFFF" points="190.3 113.1 192.3 119.1 198.7 119.1 193.5 122.9 195.5 129 190.3 125.2 185.1 129 187.1 122.9 181.9 119.1 188.3 119.1"></polygon>\
+        <polygon id="Path" fill="#FFFFFF" points="209.2 18.4 211.2 24.5 217.6 24.5 212.4 28.2 214.4 34.3 209.2 30.5 204.1 34.3 206 28.2 200.9 24.5 207.3 24.5"></polygon>\
+        <polygon id="Path" fill="#FFFFFF" points="209.2 45.2 211.2 51.3 217.6 51.3 212.4 55.1 214.4 61.1 209.2 57.4 204.1 61.1 206 55.1 200.9 51.3 207.3 51.3"></polygon>\
+        <polygon id="Path" fill="#FFFFFF" points="209.2 72.1 211.2 78.2 217.6 78.2 212.4 81.9 214.4 88 209.2 84.2 204.1 88 206 81.9 200.9 78.2 207.3 78.2"></polygon>\
+        <polygon id="Path" fill="#FFFFFF" points="209.2 98.9 211.2 105 217.6 105 212.4 108.8 214.4 114.8 209.2 111.1 204.1 114.8 206 108.8 200.9 105 207.3 105"></polygon>\
+        <polygon id="Path" fill="#FFFFFF" points="209.2 125.8 211.2 131.8 217.6 131.8 212.4 135.6 214.4 141.7 209.2 137.9 204.1 141.7 206 135.6 200.9 131.8 207.3 131.8"></polygon>\
+    </g>\
+</g>\
+</svg>\
+                    </a>\
+                </li>\
+            </ul>\
+        </div>\
+    </div>';
+     
+    $('#common-top-navbar').append(commonTopNavbar);
+    }
+        function LoadFooter() {
+            var commonFooter = '<div class="card-footer text-light navbar-dark bg-dark ">\
+            <div class="row">\
+				<div class="col-sm-2">\
+					<label class="text-light">CREATED BY:</label>\
+					<label class="text-light">Dolly</label>\
+				</div>\
+				<div class="col-sm-2">\
+					<label class="text-light">CREATED ON:</label>\
+					<label class="text-light">08/20/2019</label>\
+				</div>\
+				<div class="col-sm-2">\
+					<label class="text-light">LAST UPDATED BY:</label>\
+					<label class="text-light">Dolly123</label>\
+				</div>\
+				<div class="col-sm-2">\
+					<label class="text-light">LAST UPDATED ON:</label>\
+					<label class="text-light">08/20/2019</label>\
+				</div>\
+				<div class="col-sm-2">\
+					<label class="text-light">LOG DETAILS</label>\
+				</div>\
+			</div>\
+    </div>';
+
+    $('#common-footer').append(commonFooter);
+}
