b38f251177a410f2355edeb55edf00e4b0365c7f
James Benedict <jamesbenedict.9980@gmail.com>
2020-07-29 22:20:07 -0700

    Add files via upload

---
+++ copy-files/countertop-quiz/js/quiz.js
@@ -0,0 +1,766 @@
+var mystring = document.getElementById("data").innerHTML;
+var splits = mystring.split("/");
+//alert(splits[5]);
+var bcImage;
+bcImage = document.getElementsByClassName("bcImage")[0];
+bcImage.onclick = function () {
+	
+if(splits[0]== "family-friendly-unfussy" || splits[0]== "classic-all-the-way")
+	{
+	if(splits[1]== "1" || splits[1]== "2")
+		{
+			if(splits[3]== "cool-light" || splits[3]== "cool-dark")
+				{
+					window.location.href = 'page9.html?';
+				}
+			else if(splits[3]== "warm-light" || splits[3]== "warm-dark")
+				{
+					window.location.href = 'page10.html?';
+				}			
+		}
+	else if(splits[1]== "3" || splits[1]== "4")
+			{
+			if(splits[3]== "cool-light" || splits[3]== "cool-dark")
+				{
+					window.location.href = 'page11.html?';
+				}
+			else if(splits[3]== "warm-light" || splits[3]== "warm-dark")
+				{
+					window.location.href = 'page12.html?';
+				}				
+			}
+	}
+else if(splits[0]== "eclectic-fusion-of-styles" || splits[0]== "high-drama-colors-patterns")
+		{
+		if(splits[1]== "1" || splits[1]== "2")
+			{
+			if(splits[3]== "cool-light" || splits[3]== "cool-dark")
+				{
+					window.location.href = 'page13.html?';
+				}
+			else if(splits[3]== "warm-light" || splits[3]== "warm-dark")
+				{
+					window.location.href = 'page14.html?';
+				}
+			}
+
+		else if(splits[1]== "3" || splits[1]== "4")
+			{
+			if(splits[3]== "cool-light" || splits[3]== "cool-dark")
+				{
+					window.location.href = 'page15.html?';
+				}
+			else if(splits[3]== "warm-light" || splits[3]== "warm-dark")
+				{
+					window.location.href = 'page16.html?';
+				}
+			}
+		}
+		
+	}
+	
+	
+	
+	
+
+var mystring = document.getElementById("data").innerHTML;
+var splits = mystring.split("/");
+//alert(splits[2]);
+var bcImage;
+bcImage = document.getElementsByClassName("bcImage")[1];
+bcImage.onclick = function () {
+
+
+if(splits[0]== "family-friendly-unfussy" || splits[0]== "classic-all-the-way")
+	{
+	if(splits[1]== "1" || splits[1]== "2")
+		{
+			if(splits[3]== "cool-light" || splits[3]== "cool-dark")
+				{
+					window.location.href = 'page9.html?';
+				}
+			else if(splits[3]== "warm-light" || splits[3]== "warm-dark")
+				{
+					window.location.href = 'page10.html?';
+				}			
+		}
+	else if(splits[1]== "3" || splits[1]== "4")
+			{
+			if(splits[3]== "cool-light" || splits[3]== "cool-dark")
+				{
+					window.location.href = 'page11.html?';
+				}
+			else if(splits[3]== "warm-light" || splits[3]== "warm-dark")
+				{
+					window.location.href = 'page12.html?';
+				}				
+			}
+	}
+else if(splits[0]== "eclectic-fusion-of-styles" || splits[0]== "high-drama-colors-patterns")
+		{
+		if(splits[1]== "1" || splits[1]== "2")
+			{
+			if(splits[3]== "cool-light" || splits[3]== "cool-dark")
+				{
+					window.location.href = 'page13.html?';
+				}
+			else if(splits[3]== "warm-light" || splits[3]== "warm-dark")
+				{
+					window.location.href = 'page14.html?';
+				}
+			}
+
+		else if(splits[1]== "3" || splits[1]== "4")
+			{
+			if(splits[3]== "cool-light" || splits[3]== "cool-dark")
+				{
+					window.location.href = 'page15.html?';
+				}
+			else if(splits[3]== "warm-light" || splits[3]== "warm-dark")
+				{
+					window.location.href = 'page16.html?';
+				}
+			}
+		}
+		
+	}
+	
+	
+	
+	
+var mystring = document.getElementById("data").innerHTML;
+var splits = mystring.split("/");
+//alert(splits[2]);
+var bcImage;
+bcImage = document.getElementsByClassName("bcImage")[2];
+bcImage.onclick = function () {
+
+if(splits[0]== "family-friendly-unfussy" || splits[0]== "classic-all-the-way")
+	{
+	if(splits[1]== "1" || splits[1]== "2")
+		{
+			if(splits[3]== "cool-light" || splits[3]== "cool-dark")
+				{
+					window.location.href = 'page9.html?';
+				}
+			else if(splits[3]== "warm-light" || splits[3]== "warm-dark")
+				{
+					window.location.href = 'page10.html?';
+				}			
+		}
+	else if(splits[1]== "3" || splits[1]== "4")
+			{
+			if(splits[3]== "cool-light" || splits[3]== "cool-dark")
+				{
+					window.location.href = 'page11.html?';
+				}
+			else if(splits[3]== "warm-light" || splits[3]== "warm-dark")
+				{
+					window.location.href = 'page12.html?';
+				}				
+			}
+	}
+else if(splits[0]== "eclectic-fusion-of-styles" || splits[0]== "high-drama-colors-patterns")
+		{
+		if(splits[1]== "1" || splits[1]== "2")
+			{
+			if(splits[3]== "cool-light" || splits[3]== "cool-dark")
+				{
+					window.location.href = 'page13.html?';
+				}
+			else if(splits[3]== "warm-light" || splits[3]== "warm-dark")
+				{
+					window.location.href = 'page14.html?';
+				}
+			}
+
+		else if(splits[1]== "3" || splits[1]== "4")
+			{
+			if(splits[3]== "cool-light" || splits[3]== "cool-dark")
+				{
+					window.location.href = 'page15.html?';
+				}
+			else if(splits[3]== "warm-light" || splits[3]== "warm-dark")
+				{
+					window.location.href = 'page16.html?';
+				}
+			}
+		}
+		
+	}
+	
+var mystring = document.getElementById("data").innerHTML;
+var splits = mystring.split("/");
+//alert(splits[2]);
+var bcImage;
+bcImage = document.getElementsByClassName("bcImage")[3];
+bcImage.onclick = function () {
+
+if(splits[0]== "family-friendly-unfussy" || splits[0]== "classic-all-the-way")
+	{
+	if(splits[1]== "1" || splits[1]== "2")
+		{
+			if(splits[3]== "cool-light" || splits[3]== "cool-dark")
+				{
+					window.location.href = 'page9.html?';
+				}
+			else if(splits[3]== "warm-light" || splits[3]== "warm-dark")
+				{
+					window.location.href = 'page10.html?';
+				}			
+		}
+	else if(splits[1]== "3" || splits[1]== "4")
+			{
+			if(splits[3]== "cool-light" || splits[3]== "cool-dark")
+				{
+					window.location.href = 'page11.html?';
+				}
+			else if(splits[3]== "warm-light" || splits[3]== "warm-dark")
+				{
+					window.location.href = 'page12.html?';
+				}				
+			}
+	}
+else if(splits[0]== "eclectic-fusion-of-styles" || splits[0]== "high-drama-colors-patterns")
+		{
+		if(splits[1]== "1" || splits[1]== "2")
+			{
+			if(splits[3]== "cool-light" || splits[3]== "cool-dark")
+				{
+					window.location.href = 'page13.html?';
+				}
+			else if(splits[3]== "warm-light" || splits[3]== "warm-dark")
+				{
+					window.location.href = 'page14.html?';
+				}
+			}
+
+		else if(splits[1]== "3" || splits[1]== "4")
+			{
+			if(splits[3]== "cool-light" || splits[3]== "cool-dark")
+				{
+					window.location.href = 'page15.html?';
+				}
+			else if(splits[3]== "warm-light" || splits[3]== "warm-dark")
+				{
+					window.location.href = 'page16.html?';
+				}
+			}
+		}
+		
+	}
+
+
+var mystring = document.getElementById("data").innerHTML;
+var splits = mystring.split("/");
+//alert(splits[2]);
+var bcImage;
+bcImage = document.getElementsByClassName("bcImage")[4];
+bcImage.onclick = function () {
+
+if(splits[0]== "family-friendly-unfussy" || splits[0]== "classic-all-the-way")
+	{
+	if(splits[1]== "1" || splits[1]== "2")
+		{
+			if(splits[3]== "cool-light" || splits[3]== "cool-dark")
+				{
+					window.location.href = 'page9.html?';
+				}
+			else if(splits[3]== "warm-light" || splits[3]== "warm-dark")
+				{
+					window.location.href = 'page10.html?';
+				}			
+		}
+	else if(splits[1]== "3" || splits[1]== "4")
+			{
+			if(splits[3]== "cool-light" || splits[3]== "cool-dark")
+				{
+					window.location.href = 'page11.html?';
+				}
+			else if(splits[3]== "warm-light" || splits[3]== "warm-dark")
+				{
+					window.location.href = 'page12.html?';
+				}				
+			}
+	}
+else if(splits[0]== "eclectic-fusion-of-styles" || splits[0]== "high-drama-colors-patterns")
+		{
+		if(splits[1]== "1" || splits[1]== "2")
+			{
+			if(splits[3]== "cool-light" || splits[3]== "cool-dark")
+				{
+					window.location.href = 'page13.html?';
+				}
+			else if(splits[3]== "warm-light" || splits[3]== "warm-dark")
+				{
+					window.location.href = 'page14.html?';
+				}
+			}
+
+		else if(splits[1]== "3" || splits[1]== "4")
+			{
+			if(splits[3]== "cool-light" || splits[3]== "cool-dark")
+				{
+					window.location.href = 'page15.html?';
+				}
+			else if(splits[3]== "warm-light" || splits[3]== "warm-dark")
+				{
+					window.location.href = 'page16.html?';
+				}
+			}
+		}
+		
+	}
+
+
+
+
+var mystring = document.getElementById("data").innerHTML;
+var splits = mystring.split("/");
+//alert(splits[2]);
+var bcImage;
+bcImage = document.getElementsByClassName("bcImage")[5];
+bcImage.onclick = function () {
+
+if(splits[0]== "family-friendly-unfussy" || splits[0]== "classic-all-the-way")
+	{
+	if(splits[1]== "1" || splits[1]== "2")
+		{
+			if(splits[3]== "cool-light" || splits[3]== "cool-dark")
+				{
+					window.location.href = 'page9.html?';
+				}
+			else if(splits[3]== "warm-light" || splits[3]== "warm-dark")
+				{
+					window.location.href = 'page10.html?';
+				}			
+		}
+	else if(splits[1]== "3" || splits[1]== "4")
+			{
+			if(splits[3]== "cool-light" || splits[3]== "cool-dark")
+				{
+					window.location.href = 'page11.html?';
+				}
+			else if(splits[3]== "warm-light" || splits[3]== "warm-dark")
+				{
+					window.location.href = 'page12.html?';
+				}				
+			}
+	}
+else if(splits[0]== "eclectic-fusion-of-styles" || splits[0]== "high-drama-colors-patterns")
+		{
+		if(splits[1]== "1" || splits[1]== "2")
+			{
+			if(splits[3]== "cool-light" || splits[3]== "cool-dark")
+				{
+					window.location.href = 'page13.html?';
+				}
+			else if(splits[3]== "warm-light" || splits[3]== "warm-dark")
+				{
+					window.location.href = 'page14.html?';
+				}
+			}
+
+		else if(splits[1]== "3" || splits[1]== "4")
+			{
+			if(splits[3]== "cool-light" || splits[3]== "cool-dark")
+				{
+					window.location.href = 'page15.html?';
+				}
+			else if(splits[3]== "warm-light" || splits[3]== "warm-dark")
+				{
+					window.location.href = 'page16.html?';
+				}
+			}
+		}
+		
+	}
+
+
+
+
+
+var mystring = document.getElementById("data").innerHTML;
+var splits = mystring.split("/");
+//alert(splits[2]);
+var bcImage;
+bcImage = document.getElementsByClassName("bcImage")[6];
+bcImage.onclick = function () {
+
+if(splits[0]== "family-friendly-unfussy" || splits[0]== "classic-all-the-way")
+	{
+	if(splits[1]== "1" || splits[1]== "2")
+		{
+			if(splits[3]== "cool-light" || splits[3]== "cool-dark")
+				{
+					window.location.href = 'page9.html?';
+				}
+			else if(splits[3]== "warm-light" || splits[3]== "warm-dark")
+				{
+					window.location.href = 'page10.html?';
+				}			
+		}
+	else if(splits[1]== "3" || splits[1]== "4")
+			{
+			if(splits[3]== "cool-light" || splits[3]== "cool-dark")
+				{
+					window.location.href = 'page11.html?';
+				}
+			else if(splits[3]== "warm-light" || splits[3]== "warm-dark")
+				{
+					window.location.href = 'page12.html?';
+				}				
+			}
+	}
+else if(splits[0]== "eclectic-fusion-of-styles" || splits[0]== "high-drama-colors-patterns")
+		{
+		if(splits[1]== "1" || splits[1]== "2")
+			{
+			if(splits[3]== "cool-light" || splits[3]== "cool-dark")
+				{
+					window.location.href = 'page13.html?';
+				}
+			else if(splits[3]== "warm-light" || splits[3]== "warm-dark")
+				{
+					window.location.href = 'page14.html?';
+				}
+			}
+
+		else if(splits[1]== "3" || splits[1]== "4")
+			{
+			if(splits[3]== "cool-light" || splits[3]== "cool-dark")
+				{
+					window.location.href = 'page15.html?';
+				}
+			else if(splits[3]== "warm-light" || splits[3]== "warm-dark")
+				{
+					window.location.href = 'page16.html?';
+				}
+			}
+		}
+		
+	}
+
+
+
+
+var mystring = document.getElementById("data").innerHTML;
+var splits = mystring.split("/");
+//alert(splits[2]);
+var bcImage;
+bcImage = document.getElementsByClassName("bcImage")[7];
+bcImage.onclick = function () {
+
+if(splits[0]== "family-friendly-unfussy" || splits[0]== "classic-all-the-way")
+	{
+	if(splits[1]== "1" || splits[1]== "2")
+		{
+			if(splits[3]== "cool-light" || splits[3]== "cool-dark")
+				{
+					window.location.href = 'page9.html?';
+				}
+			else if(splits[3]== "warm-light" || splits[3]== "warm-dark")
+				{
+					window.location.href = 'page10.html?';
+				}			
+		}
+	else if(splits[1]== "3" || splits[1]== "4")
+			{
+			if(splits[3]== "cool-light" || splits[3]== "cool-dark")
+				{
+					window.location.href = 'page11.html?';
+				}
+			else if(splits[3]== "warm-light" || splits[3]== "warm-dark")
+				{
+					window.location.href = 'page12.html?';
+				}				
+			}
+	}
+else if(splits[0]== "eclectic-fusion-of-styles" || splits[0]== "high-drama-colors-patterns")
+		{
+		if(splits[1]== "1" || splits[1]== "2")
+			{
+			if(splits[3]== "cool-light" || splits[3]== "cool-dark")
+				{
+					window.location.href = 'page13.html?';
+				}
+			else if(splits[3]== "warm-light" || splits[3]== "warm-dark")
+				{
+					window.location.href = 'page14.html?';
+				}
+			}
+
+		else if(splits[1]== "3" || splits[1]== "4")
+			{
+			if(splits[3]== "cool-light" || splits[3]== "cool-dark")
+				{
+					window.location.href = 'page15.html?';
+				}
+			else if(splits[3]== "warm-light" || splits[3]== "warm-dark")
+				{
+					window.location.href = 'page16.html?';
+				}
+			}
+		}
+		
+	}
+
+
+
+
+
+var mystring = document.getElementById("data").innerHTML;
+var splits = mystring.split("/");
+//alert(splits[2]);
+var bcImage;
+bcImage = document.getElementsByClassName("bcImage")[8];
+bcImage.onclick = function () {
+
+
+if(splits[0]== "family-friendly-unfussy" || splits[0]== "classic-all-the-way")
+	{
+	if(splits[1]== "1" || splits[1]== "2")
+		{
+			if(splits[3]== "cool-light" || splits[3]== "cool-dark")
+				{
+					window.location.href = 'page9.html?';
+				}
+			else if(splits[3]== "warm-light" || splits[3]== "warm-dark")
+				{
+					window.location.href = 'page10.html?';
+				}			
+		}
+	else if(splits[1]== "3" || splits[1]== "4")
+			{
+			if(splits[3]== "cool-light" || splits[3]== "cool-dark")
+				{
+					window.location.href = 'page11.html?';
+				}
+			else if(splits[3]== "warm-light" || splits[3]== "warm-dark")
+				{
+					window.location.href = 'page12.html?';
+				}				
+			}
+	}
+else if(splits[0]== "eclectic-fusion-of-styles" || splits[0]== "high-drama-colors-patterns")
+		{
+		if(splits[1]== "1" || splits[1]== "2")
+			{
+			if(splits[3]== "cool-light" || splits[3]== "cool-dark")
+				{
+					window.location.href = 'page13.html?';
+				}
+			else if(splits[3]== "warm-light" || splits[3]== "warm-dark")
+				{
+					window.location.href = 'page14.html?';
+				}
+			}
+
+		else if(splits[1]== "3" || splits[1]== "4")
+			{
+			if(splits[3]== "cool-light" || splits[3]== "cool-dark")
+				{
+					window.location.href = 'page15.html?';
+				}
+			else if(splits[3]== "warm-light" || splits[3]== "warm-dark")
+				{
+					window.location.href = 'page16.html?';
+				}
+			}
+		}
+		
+	}
+
+
+
+
+
+var mystring = document.getElementById("data").innerHTML;
+var splits = mystring.split("/");
+//alert(splits[2]);
+var bcImage;
+bcImage = document.getElementsByClassName("bcImage")[9];
+bcImage.onclick = function () {
+
+if(splits[0]== "family-friendly-unfussy" || splits[0]== "classic-all-the-way")
+	{
+	if(splits[1]== "1" || splits[1]== "2")
+		{
+			if(splits[3]== "cool-light" || splits[3]== "cool-dark")
+				{
+					window.location.href = 'page9.html?';
+				}
+			else if(splits[3]== "warm-light" || splits[3]== "warm-dark")
+				{
+					window.location.href = 'page10.html?';
+				}			
+		}
+	else if(splits[1]== "3" || splits[1]== "4")
+			{
+			if(splits[3]== "cool-light" || splits[3]== "cool-dark")
+				{
+					window.location.href = 'page11.html?';
+				}
+			else if(splits[3]== "warm-light" || splits[3]== "warm-dark")
+				{
+					window.location.href = 'page12.html?';
+				}				
+			}
+	}
+else if(splits[0]== "eclectic-fusion-of-styles" || splits[0]== "high-drama-colors-patterns")
+		{
+		if(splits[1]== "1" || splits[1]== "2")
+			{
+			if(splits[3]== "cool-light" || splits[3]== "cool-dark")
+				{
+					window.location.href = 'page13.html?';
+				}
+			else if(splits[3]== "warm-light" || splits[3]== "warm-dark")
+				{
+					window.location.href = 'page14.html?';
+				}
+			}
+
+		else if(splits[1]== "3" || splits[1]== "4")
+			{
+			if(splits[3]== "cool-light" || splits[3]== "cool-dark")
+				{
+					window.location.href = 'page15.html?';
+				}
+			else if(splits[3]== "warm-light" || splits[3]== "warm-dark")
+				{
+					window.location.href = 'page16.html?';
+				}
+			}
+		}
+		
+	}
+
+
+
+
+
+var mystring = document.getElementById("data").innerHTML;
+var splits = mystring.split("/");
+//alert(splits[2]);
+var bcImage;
+bcImage = document.getElementsByClassName("bcImage")[10];
+bcImage.onclick = function () {
+
+if(splits[0]== "family-friendly-unfussy" || splits[0]== "classic-all-the-way")
+	{
+	if(splits[1]== "1" || splits[1]== "2")
+		{
+			if(splits[3]== "cool-light" || splits[3]== "cool-dark")
+				{
+					window.location.href = 'page9.html?';
+				}
+			else if(splits[3]== "warm-light" || splits[3]== "warm-dark")
+				{
+					window.location.href = 'page10.html?';
+				}			
+		}
+	else if(splits[1]== "3" || splits[1]== "4")
+			{
+			if(splits[3]== "cool-light" || splits[3]== "cool-dark")
+				{
+					window.location.href = 'page11.html?';
+				}
+			else if(splits[3]== "warm-light" || splits[3]== "warm-dark")
+				{
+					window.location.href = 'page12.html?';
+				}				
+			}
+	}
+else if(splits[0]== "eclectic-fusion-of-styles" || splits[0]== "high-drama-colors-patterns")
+		{
+		if(splits[1]== "1" || splits[1]== "2")
+			{
+			if(splits[3]== "cool-light" || splits[3]== "cool-dark")
+				{
+					window.location.href = 'page13.html?';
+				}
+			else if(splits[3]== "warm-light" || splits[3]== "warm-dark")
+				{
+					window.location.href = 'page14.html?';
+				}
+			}
+
+		else if(splits[1]== "3" || splits[1]== "4")
+			{
+			if(splits[3]== "cool-light" || splits[3]== "cool-dark")
+				{
+					window.location.href = 'page15.html?';
+				}
+			else if(splits[3]== "warm-light" || splits[3]== "warm-dark")
+				{
+					window.location.href = 'page16.html?';
+				}
+			}
+		}
+		
+	}
+
+
+
+
+var mystring = document.getElementById("data").innerHTML;
+var splits = mystring.split("/");
+//alert(splits[2]);
+var bcImage;
+bcImage = document.getElementsByClassName("bcImage")[11];
+bcImage.onclick = function () {
+
+if(splits[0]== "family-friendly-unfussy" || splits[0]== "classic-all-the-way")
+	{
+	if(splits[1]== "1" || splits[1]== "2")
+		{
+			if(splits[3]== "cool-light" || splits[3]== "cool-dark")
+				{
+					window.location.href = 'page9.html?';
+				}
+			else if(splits[3]== "warm-light" || splits[3]== "warm-dark")
+				{
+					window.location.href = 'page10.html?';
+				}			
+		}
+	else if(splits[1]== "3" || splits[1]== "4")
+			{
+			if(splits[3]== "cool-light" || splits[3]== "cool-dark")
+				{
+					window.location.href = 'page11.html?';
+				}
+			else if(splits[3]== "warm-light" || splits[3]== "warm-dark")
+				{
+					window.location.href = 'page12.html?';
+				}				
+			}
+	}
+else if(splits[0]== "eclectic-fusion-of-styles" || splits[0]== "high-drama-colors-patterns")
+		{
+		if(splits[1]== "1" || splits[1]== "2")
+			{
+			if(splits[3]== "cool-light" || splits[3]== "cool-dark")
+				{
+					window.location.href = 'page13.html?';
+				}
+			else if(splits[3]== "warm-light" || splits[3]== "warm-dark")
+				{
+					window.location.href = 'page14.html?';
+				}
+			}
+
+		else if(splits[1]== "3" || splits[1]== "4")
+			{
+			if(splits[3]== "cool-light" || splits[3]== "cool-dark")
+				{
+					window.location.href = 'page15.html?';
+				}
+			else if(splits[3]== "warm-light" || splits[3]== "warm-dark")
+				{
+					window.location.href = 'page16.html?';
+				}
+			}
+		}
+		
+	}
